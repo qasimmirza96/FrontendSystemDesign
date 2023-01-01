de240b8de6c754a4c8a09f613c6a9d94ca157edc
qasimmirza96 <freeused4@gmail.com>
2023-01-01 09:21:38 +0000

    ci: update workflow

--- generate-history.js
+++ generate-history.js
@@ -8,47 +8,105 @@ const args = process.argv.slice(2);
 
 function getArg(name, defaultValue) {
     const index = args.indexOf(`--${name}`);
-    if (index !== -1 && args[index + 1]) {
-        return args[index + 1];
-    }
-    return defaultValue;
+    return index !== -1 && args[index + 1]
+        ? args[index + 1]
+        : defaultValue;
 }
 
-const YEAR = parseInt(getArg("year", "2023"));
+function hasFlag(flag) {
+    return args.includes(`--${flag}`);
+}
+
+const YEAR = parseInt(getArg("year", new Date().getFullYear()));
+
+const START = getArg("start", `${YEAR}-01-01`);
+const END = getArg("end", `${YEAR}-12-31`);
 
 const MIN_COMMITS = parseInt(getArg("min", "2"));
-const MAX_COMMITS = parseInt(getArg("max", "2"));
+const MAX_COMMITS = parseInt(getArg("max", "10"));
+
+const SKIP_PERCENT = parseInt(getArg("skip", "0"));
+
+const WEEKEND_MODE = hasFlag("weekend");
 
 const FILE = path.join(__dirname, "history.json");
+
+const COMMIT_MESSAGES = [
+    "feat: add new feature",
+    "fix: resolve issue",
+    "refactor: improve code",
+    "docs: update docs",
+    "style: formatting",
+    "test: add tests",
+    "build: update project",
+    "perf: optimize",
+    "chore: maintenance",
+    "ci: update workflow"
+];
 
 function random(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function randomTime(date) {
-    const hour = random(8, 22);
-    const minute = random(0, 59);
-    const second = random(0, 59);
+function randomItem(arr) {
+    return arr[random(0, arr.length - 1)];
+}
 
+function randomTime(date) {
     return new Date(
         date.getFullYear(),
         date.getMonth(),
         date.getDate(),
-        hour,
-        minute,
-        second
+        random(8, 22),
+        random(0, 59),
+        random(0, 59)
     );
 }
 
+const startDate = new Date(`${START}T00:00:00`);
+const endDate = new Date(`${END}T00:00:00`);
+
 let commitsCreated = 0;
+let skippedDays = 0;
+
+const totalDays =
+    Math.floor((endDate - startDate) / (1000 * 60 * 60 * 24)) + 1;
+
+let processed = 0;
+
+console.log(`Generating commits...`);
 
 for (
-    let current = new Date(YEAR, 0, 1);
-    current.getFullYear() === YEAR;
+    let current = new Date(startDate);
+    current <= endDate;
     current.setDate(current.getDate() + 1)
 ) {
+    processed++;
 
-    const commitsToday = random(MIN_COMMITS, MAX_COMMITS);
+    process.stdout.write(
+        `\rDay ${processed}/${totalDays} | Commits: ${commitsCreated}`
+    );
+
+    // Skip chance
+    if (random(1, 100) <= SKIP_PERCENT) {
+        skippedDays++;
+        continue;
+    }
+
+    let min = MIN_COMMITS;
+    let max = MAX_COMMITS;
+
+    if (WEEKEND_MODE) {
+        const day = current.getDay();
+
+        // Sunday or Saturday
+        if (day === 0 || day === 6) {
+            min = 0;
+            max = Math.min(3, MAX_COMMITS);
+        }
+    }
+
+    const commitsToday = random(min, max);
 
     for (let i = 0; i < commitsToday; i++) {
 
@@ -64,11 +122,11 @@ for (
 
         execSync("git add .");
 
-        execSync(`git commit -m "Daily Update"`, {
+        execSync(`git commit -m "${randomItem(COMMIT_MESSAGES)}"`, {
             env: {
                 ...process.env,
                 GIT_AUTHOR_DATE: commitDate.toISOString(),
-                GIT_COMMITTER_DATE: commitDate.toISOString(),
+                GIT_COMMITTER_DATE: commitDate.toISOString()
             },
             stdio: "ignore"
         });
@@ -77,6 +135,13 @@ for (
     }
 }
 
-console.log(`Finished!`);
-console.log(`Year: ${YEAR}`);
-console.log(`Total commits: ${commitsCreated}`);
+console.log("\n");
+console.log("=================================");
+console.log("Finished!");
+console.log("=================================");
+console.log(`Year           : ${YEAR}`);
+console.log(`Start          : ${START}`);
+console.log(`End            : ${END}`);
+console.log(`Skipped Days   : ${skippedDays}`);
+console.log(`Total Commits  : ${commitsCreated}`);
+console.log("=================================");
